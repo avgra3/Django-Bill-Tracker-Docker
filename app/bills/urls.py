6f39766f2448c21718cce481c700e1b19514fb9a
Antony G <antonygradillas@gmail.com>
2021-10-11 18:02:29 -0700

    Updated to add new items

--- app/bills/urls.py
+++ app/bills/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.homepage, name='pages/home.html'),
     path('avg/', views.AverageBillPaidView, name='bills-avg'),
     path('mb/', views.MonthlyBreakdownListView, name='bills-mb'),
-    path('new-bill/', views.NewBillView, name='bills-bill-form'),
-    path('new-carrier/', views.NewCarrierView, name='bills-carrier-form'),
-    path('new-product/', views.NewProductView, name='bills-product-form'),
     path('bills/bills/', BillListView.as_view(), name='bills-paid'),
+    path('bills/create/bill/', BillCreateView.as_view(), name='bills-create-bill'),
+    path('bills/create/product/', ProductCreateView.as_view(), name='bills-create-product'),
+    path('bills/create/carrier/', CarrierCreateView.as_view(), name='bills-create-carrier'),
 ]
--- app/bills/views.py
+++ app/bills/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.core import serializers
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.db.models import Sum, Count, Avg, Q, Count, Case, When, F
 from django.db.models.functions import TruncMonth
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Models created
-from .models import BillPaid, Carrier, Bill
+from .models import BillPaid, Carrier, Bill, Product
 
 # Forms created
 from .forms import NewBillForm, NewCarrierForm, NewProductForm
@@ -19,10 +19,13 @@ def homepage(request):
     # Shows any unpaid bills:
     unpaid = BillPaid.objects.all().filter(paidBool=0).values('billID', 'totalPaid', 'notes')
     
+    # Shows summed amount for total due
+    unpaid_total = BillPaid.objects.all().filter(paidBool=0).aggregate(Sum('totalPaid'))
+    
     # Show previous 5 bills
     paid = BillPaid.objects.all().select_related('billID').order_by('-paidDate')[:5]
 
-    context = {"unpaid": unpaid, "paid":paid}
+    context = {"unpaid": unpaid, "paid":paid, "unpaid_total": unpaid_total}
     return render(request, 'pages/home.html', context=context)
 
 # Average bill view
@@ -46,27 +49,36 @@ def MonthlyBreakdownListView(request):
     
     return render(request = request, template_name='pages/bills-mb.html', context={"mb": context})
 
-# New Bill Form view
-def NewBillView(request):
-    context = NewBillForm()
-    return render(request = request, template_name='pages/bills-bill-form.html', context={"form":context})
+""" Adding New Bills, Carriers, Products, etc. """
+class BillCreateView(LoginRequiredMixin, CreateView):
+    model = Bill
+    fields = "__all__"
+    template_name = 'pages/bill-form.html'
 
-# New Carrier Form view
-def NewCarrierView(request):
-    context = NewCarrierForm()
-    return render(request = request, template_name='pages/bills-carrier-form.html', context={"form":context})
+    def form_valid(self, form):
+        return redirect('bills-paid')
 
-# New Product Form view
-def NewProductView(request):
-    context = NewProductForm()
-    return render(request = request, template_name='pages/bills-product-form.html', context={"form":context})
+class CarrierCreateView(LoginRequiredMixin, CreateView):
+    model = Carrier
+    fields = "__all__"
+    template_name = 'pages/carrier-form.html'
 
-"""
-Detail view to see more details of the bill...
-"""
+    def form_valid(self, form):
+        return redirect('pages/home.html')
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    fields = "__all__"
+    template_name = 'pages/product-form.html'
+
+    def form_valid(self, form):
+        return redirect('pages/home.html')
+
+""" Listing all available bills in reverse order """
 class BillListView(ListView):
     model = BillPaid
     # format: <app>/<model>_<viewtype>.html
     template_name = 'pages/BillPaid_listview.html'
     context_object_name = 'bills'
     ordering = ['-paidDate']
+
