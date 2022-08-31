d5da781f41c0e355b40aeeb92529ae77c3ea2f28
Antony <antonygradillas@gmail.com>
2022-08-31 11:38:03 -0700

    Updated homepage view

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,79 +14,80 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%ig6#2#$d)ios3djgu*w0bdae#&5ca^z*$@^7zauw5@)tu!$m'
+SECRET_KEY = "django-insecure--%ig6#2#$d)ios3djgu*w0bdae#&5ca^z*$@^7zauw5@)tu!$m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Default to localhost
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bills.apps.BillsConfig',
-    'users.apps.UsersConfig',
+    "crispy_forms",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "bills.apps.BillsConfig",
+    "users.apps.UsersConfig",
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bills',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'db',
-        'PORT': 3306,
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "bills",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "db",
+        "PORT": 3306,
     }
 }
 
@@ -96,16 +97,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -113,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -127,11 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = 'bills/home.html'
+LOGIN_REDIRECT_URL = "bills/home.html"
--- app/bills/views.py
+++ app/bills/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.core import serializers
 from django.views.generic import ListView, DetailView, CreateView
-from django.db.models import Sum, Count, Avg, Q, Count, Case, When, F
+from django.db.models import Sum, Count, Avg, Q, Count, Case, When, F, Func
 from django.db.models.functions import TruncMonth
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    FormView,
+)
 from django.urls import reverse_lazy
 
 # Import users views
@@ -18,99 +25,174 @@ from .models import BillPaid, Carrier, Bill, Product
 # Forms created
 from .forms import NewBillForm, NewCarrierForm, NewProductForm
 
+# Class to have output rounded, used for averages
+class Round(Func):
+    function = "ROUND"
+    template = "%(function)s(%(expressions)s, 2)"
+
+
 # Homepage view.
 def homepage(request):
     # Shows any unpaid bills:
-    unpaid = BillPaid.objects.all().filter(paidBool=0).values('billID', 'totalPaid', 'notes')
-    
-    # Shows summed amount for total due
-    unpaid_total = BillPaid.objects.all().filter(paidBool=0).aggregate(Sum('totalPaid'))
-    
-    # Show previous 5 bills
-    paid = BillPaid.objects.all().select_related('billID').order_by('-paidDate')[:5]
+    unpaid = (
+        BillPaid.objects.all().filter(paidBool=0).values("billID", "totalPaid", "notes")
+    )
 
-    context = {"unpaid": unpaid, "paid":paid, "unpaid_total": unpaid_total}
-    return render(request, 'bills/home.html', context=context)
+    # Shows summed amount for total due
+    unpaid_total = BillPaid.objects.all().filter(paidBool=0).aggregate(Sum("totalPaid"))
+
+    # Show previous 5 bills
+    paid = BillPaid.objects.all().select_related("billID").order_by("-paidDate")[:5]
+
+    # Group by, average each carrier type
+    averages = (
+        Bill.objects.select_related("carrierID")
+        .values("carrierID")
+        .annotate(avgCarrierAmount=Round(Avg(F("charge") + F("anc_fees") + F("taxes"))))
+        .order_by("carrierID")
+        .values("carrierID", "avgCarrierAmount")
+    )
+
+    carriers = (
+        Carrier.objects.values("carrierId", "carrierName")
+        .annotate(carrierID=F("carrierId"))
+        .values("carrierID", "carrierName")
+    )
+
+    averages_dict = list(averages)
+    carriers_dict = list(carriers)
+
+    # Creates a list of dictionaries which combines the Averages and Carriers queries
+    combined = []
+    for i in range(len(averages_dict)):
+        combined.append(averages_dict[i] | carriers_dict[i])
+        print(combined)
+
+    # Total average amount
+    averages_total = (
+        Bill.objects.values("carrierID")
+        .annotate(avgCarrierAmount=Round(Avg(F("charge") + F("anc_fees") + F("taxes"))))
+        .aggregate(Sum("avgCarrierAmount"))
+    )
+
+    context = {
+        "unpaid": unpaid,
+        "paid": paid,
+        "unpaid_total": unpaid_total,
+        "averages": averages,
+        "averages_total": averages_total,
+        "combinedAvg": combined,
+    }
+    return render(request, "bills/home.html", context=context)
+
 
 # Average bill view
 def AverageBillPaidView(request):
     # Get the running average billed amount for all bills
-    runningAvg = BillPaid.objects.aggregate(Ravg=Avg('totalPaid'))
+    runningAvg = BillPaid.objects.aggregate(Ravg=Avg("totalPaid"))
 
     # We want to get the average cost for each carrier/utility
-    monthlyAvg = BillPaid.objects.annotate(month=TruncMonth('paidDate')).values('month').annotate(a = Avg('totalPaid')).values('month', 'a').order_by('month')
+    monthlyAvg = (
+        BillPaid.objects.annotate(month=TruncMonth("paidDate"))
+        .values("month")
+        .annotate(a=Avg("totalPaid"))
+        .values("month", "a")
+        .order_by("month")
+    )
 
     # Combines the two items we want to show in view
-    context = {"runningAvg": runningAvg,  "monthlyAvg": monthlyAvg}
+    context = {"runningAvg": runningAvg, "monthlyAvg": monthlyAvg}
 
-    return render(request = request, template_name='bills/bills-avg.html', context=context)
+    return render(
+        request=request, template_name="bills/bills-avg.html", context=context
+    )
+
 
 # Monthly Breakdown view
 # Create a view that summarizes the bills table
 def MonthlyBreakdownListView(request):
     # Will return a grouped breakdown for each month
-    context = BillPaid.objects.annotate(month=TruncMonth('paidDate')).values('month').annotate(sums=Sum('totalPaid')).values('month','sums').order_by('month')
-    
-    return render(request = request, template_name='bills/bills-mb.html', context={"mb": context})
+    context = (
+        BillPaid.objects.annotate(month=TruncMonth("paidDate"))
+        .values("month")
+        .annotate(sums=Sum("totalPaid"))
+        .values("month", "sums")
+        .order_by("month")
+    )
+
+    return render(
+        request=request, template_name="bills/bills-mb.html", context={"mb": context}
+    )
+
 
 """ Adding New Bills, Carriers, Products, etc. """
+
+
 class BillCreateView(LoginRequiredMixin, CreateView):
     form_class = NewBillForm
-    template_name = 'bills/bill-form.html'
+    template_name = "bills/bill-form.html"
 
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
 
-    success_url = reverse_lazy('bills/home.html')
+    success_url = reverse_lazy("bills/home.html")
 
     def form_valid(self, form):
         return super().form_valid(form)
+
 
 class CarrierCreateView(LoginRequiredMixin, CreateView):
     form_class = NewCarrierForm
-    template_name = 'bills/carrier-form.html'
+    template_name = "bills/carrier-form.html"
 
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
 
-    success_url = reverse_lazy('bills/home.html')
+    success_url = reverse_lazy("bills/home.html")
 
     def form_valid(self, form):
         return super().form_valid(form)
+
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     form_class = NewProductForm
-    template_name = 'bills/product-form.html'
+    template_name = "bills/product-form.html"
 
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
 
-    success_url = reverse_lazy('bills/home.html')
+    success_url = reverse_lazy("bills/home.html")
 
     def form_valid(self, form):
         return super().form_valid(form)
 
+
 """ Listing all available bills in reverse order """
+
+
 class BillListView(ListView):
     model = BillPaid
     # format: <app>/<model>_<viewtype>.html
-    template_name = 'bills/BillPaid_listview.html'
-    context_object_name = 'bills'
-    ordering = ['paidDate']
+    template_name = "bills/BillPaid_listview.html"
+    context_object_name = "bills"
+    ordering = ["paidDate"]
+
 
 """ Paying unpaid bills """
+
+
 class paidBillDetailView(LoginRequiredMixin, DetailView):
     model = BillPaid
-    fields = '__all__'
+    fields = "__all__"
+
 
 class paidBillUpdateView(LoginRequiredMixin, UpdateView):
     model = BillPaid
-    fields = '__all__'
-    success_url = reverse_lazy('bills/home.html')
+    fields = "__all__"
+    success_url = reverse_lazy("bills/home.html")
 
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
 
     def form_valid(self, form):
         return super().form_valid(form)
-
