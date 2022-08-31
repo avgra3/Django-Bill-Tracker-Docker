71b69558fc4cea345f5fe3f43de22a075a967b56
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2022-08-31 14:30:05 -0700

    Removed .dbdata from git commits

--- app/bills/views.py
+++ app/bills/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.views.generic import ListView, DetailView, CreateView
 from django.db.models import Sum, Count, Avg, Q, Count, Case, When, F, Func
-from django.db.models.functions import TruncMonth
+from django.db.models.functions import TruncMonth, Extract
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import (
@@ -30,6 +30,14 @@ class Round(Func):
     function = "ROUND"
     template = "%(function)s(%(expressions)s, 2)"
 
+
+"""
+# Class to have month-year
+class MonthYear(Func):
+    function1 = "MONTH"
+    function2 = "YEAR"
+    template = "%(function1)s(%(expressions)s) - %(function2)s(%(expressions)s)"
+"""
 
 # Homepage view.
 def homepage(request):
@@ -89,16 +97,36 @@ def homepage(request):
 # Average bill view
 def AverageBillPaidView(request):
     # Get the running average billed amount for all bills
-    runningAvg = BillPaid.objects.aggregate(Ravg=Avg("totalPaid"))
-
-    # We want to get the average cost for each carrier/utility
-    monthlyAvg = (
-        BillPaid.objects.annotate(month=TruncMonth("paidDate"))
-        .values("month")
-        .annotate(a=Avg("totalPaid"))
-        .values("month", "a")
-        .order_by("month")
+    runningAvg = (
+        Bill.objects.values("carrierID")
+        .annotate(avgCarrierAmount=Round(Avg(F("charge") + F("anc_fees") + F("taxes"))))
+        .aggregate(totalavgCarrierAmount=Sum("avgCarrierAmount"))
     )
+
+    # We want to get the average cost for each carrier/utility, by year and month
+    monthlyAvg = (
+        Bill.objects.select_related("carrierID")
+        .annotate(month=Extract("dueDate", "month"), year=Extract("dueDate", "year"))
+        .values("carrierID", "year", "month")
+        .annotate(avgCarrierAmount=Round(Avg(F("charge") + F("anc_fees") + F("taxes"))))
+        .order_by("carrierID")
+        .values("year", "month", "carrierID", "avgCarrierAmount")
+        .order_by("year", "month")
+    )
+
+    carriers = (
+        Carrier.objects.values("carrierId", "carrierName")
+        .annotate(carrierID=F("carrierId"))
+        .values("carrierID", "carrierName")
+    )
+
+    monthlyAvg_dict = list(monthlyAvg)
+    carriers_dict = list(carriers)
+
+    # Creates a list of dictionaries which combines the Averages and Carriers queries
+    monthlyAvg = []
+    for i in range(len(monthlyAvg_dict)):
+        monthlyAvg.append(monthlyAvg_dict[i] | carriers_dict[i % len(carriers_dict)])
 
     # Combines the two items we want to show in view
     context = {"runningAvg": runningAvg, "monthlyAvg": monthlyAvg}
@@ -167,7 +195,7 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-""" Listing all available bills in reverse order """
+""" Listing all available bills in order """
 
 
 class BillListView(ListView):
