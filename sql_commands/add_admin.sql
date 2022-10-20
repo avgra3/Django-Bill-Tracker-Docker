00019ac4fb02a6c8a7af21418b321f9e56f51206
Antony <antonygradillas@gmail.com>
2022-10-20 10:20:56 -0700

    Added reverse proxy and reformatted

--- sql_commands/add_admin.sql
+++
@@ -1,7 +0,0 @@
-/* This allows us to access the database externally */
-
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
