c0c30635922e43dbc4c7cc1a8dc321aba341b282
Antony <antonygradillas@gmail.com>
2022-11-04 11:22:04 -0700

    Link to upload admin page on main & smooth scroll

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
+gunicorn app.wsgi:application --bind 0.0.0.0:8000 --reload
