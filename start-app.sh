09dba974d3673dff7cae97844ff8892c8afaaf9d
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2022-11-04 12:18:17 -0700

    Development (#24)
    * Added csv upload to admin page
    
    * Link to upload admin page on main & smooth scroll
    
    * Added thousands seperator to settings file

--- start-app.sh
+++ start-app.sh
@@ -4,6 +4,6 @@
 # docker-compose up --force-recreate --remove-orphans --abort-on-container-exit --always-recreate-deps
 
 # Runs commands in the background
-docker-compose build --no-cache
+docker-compose build #--no-cache
 #docker-compose up -d --force-recreate
 docker-compose up --force-recreate  --always-recreate-deps
